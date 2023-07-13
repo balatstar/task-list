cce47aa0cabf7b4a9b77fbad620d0e6d5cc355b1
joyce <joyce.ann.cruz@gmail.com>
2023-07-14 02:04:38 +0800

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import './style.css';
 import More from './more.png';
 import Delete from './delete.png';
 
-const postContainer = document.querySelector('.tasklist');
-const addButton = document.querySelector('.newtask-return');
-const addNewInput = document.getElementById('addnew');
-
 function getItems() {
   const value = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
   return value;
@@ -20,47 +16,18 @@ function refreshList() {
   postContainer.innerHTML = '';
 
   items.forEach((item, index) => {
-    const itemElement = document.createElement('div');
-    itemElement.classList.add('task-group');
-    itemElement.innerHTML = `
-      <input class="task-item" type="checkbox">
-      <input class="task-item-text" placeholder="Task item...">
-    `;
-
-    const myIcon = new Image();
-    myIcon.src = More;
-    myIcon.classList.add('task-item-more');
-    itemElement.appendChild(myIcon);
-
-    const myDelete = new Image();
-    myDelete.src = Delete;
-    myDelete.classList.add('task-item-delete');
-    itemElement.appendChild(myDelete);
-
-    const descriptionInput = itemElement.querySelector('.task-item-text');
-    const completedInput = itemElement.querySelector('.task-item');
-    const deleteButton = itemElement.querySelector('.task-item-delete');
-
-    descriptionInput.value = item.description;
-    completedInput.checked = item.completed;
-
-    descriptionInput.addEventListener('change', () => {
-      updateItem(item, 'description', descriptionInput.value);
-    });
-
-    completedInput.addEventListener('change', () => {
-      updateItem(item, 'completed', completedInput.checked);
-    });
-
-    deleteButton.addEventListener('click', () => {
-      deleteItem(index);
-    });
-
+    const itemElement = createItemElement(item, index);
     postContainer.appendChild(itemElement);
   });
 }
 
+const postContainer = document.querySelector('.tasklist');
+const addButton = document.querySelector('.newtask-return');
+
+const items = getItems();
+
 function addItem() {
+  const addNewInput = document.getElementById('addnew');
   const description = addNewInput.value;
 
   items.push({
@@ -86,20 +53,57 @@ function deleteItem(index) {
   refreshList();
 }
 
-let items = getItems();
+function createItemElement(item, index) {
+  const itemElement = document.createElement('div');
+  itemElement.classList.add('task-group');
+  itemElement.innerHTML = `
+    <input class="task-item" type="checkbox">
+    <input class="task-item-text" placeholder="Task item...">
+  `;
 
-function handleAddButtonClick() {
-  addItem();
+  const myIcon = new Image();
+  myIcon.src = More;
+  myIcon.classList.add('task-item-more');
+  itemElement.appendChild(myIcon);
+
+  const myDelete = new Image();
+  myDelete.src = Delete;
+  myDelete.classList.add('task-item-delete');
+  itemElement.appendChild(myDelete);
+
+  const descriptionInput = itemElement.querySelector('.task-item-text');
+  const completedInput = itemElement.querySelector('.task-item');
+  const deleteButton = itemElement.querySelector('.task-item-delete');
+
+  descriptionInput.value = item.description;
+  completedInput.checked = item.completed;
+
+  descriptionInput.addEventListener('change', () => {
+    updateItem(item, 'description', descriptionInput.value);
+  });
+
+  completedInput.addEventListener('change', () => {
+    updateItem(item, 'completed', completedInput.checked);
+  });
+
+  deleteButton.addEventListener('click', () => {
+    deleteItem(index);
+  });
+
+  return itemElement;
 }
 
-function handleAddNewInputKeyDown(event) {
+addButton.addEventListener('click', () => {
+  addItem();
+});
+
+const addNewInput = document.getElementById('addnew');
+
+addNewInput.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     event.preventDefault(); // Prevent the default form submission behavior
     addItem();
   }
-}
-
-addButton.addEventListener('click', handleAddButtonClick);
-addNewInput.addEventListener('keydown', handleAddNewInputKeyDown);
+});
 
 refreshList();
