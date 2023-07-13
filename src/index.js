0ecb80bccc07004c2873f31cb7bb9a513b06d6d3
joyce <joyce.ann.cruz@gmail.com>
2023-07-14 00:31:33 +0800

    Implement create, remove, update and delete functionality

--- src/index.js
+++ src/index.js
@@ -1,44 +1,103 @@
 import _ from 'lodash';
 import './style.css';
 import More from './more.png';
+import Delete from './delete.png';
 
-const cardData = [
-  {
-    description: 'Wash the dishes',
-    completed: false,
-    index: '3',
-  },
-  {
-    description: 'Complete the project',
-    completed: false,
-    index: '2',
-  },
-  {
-    description: 'Submit for code review',
-    completed: false,
-    index: '1',
-  },
-];
+function getItems() {
+  const value = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+  return value;
+}
+
+function setItems(items) {
+  const itemsJson = JSON.stringify(items);
+  localStorage.setItem('tasks', itemsJson);
+}
 
 const postContainer = document.querySelector('.tasklist');
+const addButton = document.querySelector('.newtask-return');
 
-const postMethods = () => {
-  // Sort the cardData array by the index value in ascending order
-  const sortedData = _.sortBy(cardData, [(data) => parseInt(data.index, 10)]);
+let items = getItems();
 
-  sortedData.forEach((postData) => {
-    const postElement = document.createElement('div');
-    postElement.classList.add('task-group');
-    postElement.innerHTML = `
-        <input id="${postData.index}" type="checkbox" name="task-item" value="${postData.index}"> <input id="input-${postData.index}" class="task-item-text" placeholder="${postData.description}">
-      `;
+function addItem() {
+  const addNewInput = document.getElementById('addnew');
+const description = addNewInput.value;
+
+items.push({
+  description: description,
+  completed: false
+});
+
+setItems(items);
+refreshList();
+
+addNewInput.value = ''; // Clear the input field after adding the item
+}
+
+function updateItem(item, key, value) {
+  item[key] = value;
+  setItems(items);
+  refreshList();
+}
+
+function refreshList() {
+  postContainer.innerHTML = '';
+
+  for (const item of items) {
+    const itemElement = document.createElement('div');
+    itemElement.classList.add('task-group');
+    itemElement.innerHTML = `
+      <input class="task-item" type="checkbox">
+      <input class="task-item-text" placeholder="Task item...">
+    `;
+
     const myIcon = new Image();
     myIcon.src = More;
     myIcon.classList.add('task-item-more');
-    postElement.appendChild(myIcon);
+    itemElement.appendChild(myIcon);
 
-    postContainer.appendChild(postElement);
-  });
-};
+    const myDelete = new Image();
+    myDelete.src = Delete;
+    myDelete.classList.add('task-item-delete');
+    itemElement.appendChild(myDelete);
 
-postMethods();
+    const descriptionInput = itemElement.querySelector('.task-item-text');
+    const completedInput = itemElement.querySelector('.task-item');
+    const deleteButton = itemElement.querySelector('.task-item-delete');
+
+    descriptionInput.value = item.description;
+    completedInput.checked = item.completed;
+
+    descriptionInput.addEventListener('change', () => {
+      updateItem(item, 'description', descriptionInput.value);
+    });
+
+    completedInput.addEventListener('change', () => {
+      updateItem(item, 'completed', completedInput.checked);
+    });
+
+    deleteButton.addEventListener('click', () => {
+      items = items.filter((i) => i !== item);
+      setItems(items);
+      refreshList();
+    });
+
+    postContainer.appendChild(itemElement);
+  }
+}
+
+
+addButton.addEventListener('click', () => {
+  addItem();
+})
+
+const addNewInput = document.getElementById('addnew');
+
+addNewInput.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    event.preventDefault(); // Prevent the default form submission behavior
+    addItem();
+  }
+});
+
+
+refreshList();
