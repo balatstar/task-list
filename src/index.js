f99f6d9561c1992ad1f640f35231efd3f9e2363d
joyce <joyce.ann.cruz@gmail.com>
2023-07-14 01:07:52 +0800

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import './style.css';
 import More from './more.png';
 import Delete from './delete.png';
@@ -20,17 +19,17 @@ let items = getItems();
 
 function addItem() {
   const addNewInput = document.getElementById('addnew');
-const description = addNewInput.value;
+  const description = addNewInput.value;
 
-items.push({
-  description: description,
-  completed: false
-});
+  items.push({
+    description,
+    completed: false,
+  });
 
-setItems(items);
-refreshList();
+  setItems(items);
+  refreshList();
 
-addNewInput.value = ''; // Clear the input field after adding the item
+  addNewInput.value = ''; // Clear the input field after adding the item
 }
 
 function updateItem(item, key, value) {
@@ -39,65 +38,67 @@ function updateItem(item, key, value) {
   refreshList();
 }
 
+function deleteItem(index) {
+  items.splice(index, 1);
+  setItems(items);
+  refreshList();
+}
+
 function refreshList() {
   postContainer.innerHTML = '';
 
-  for (const item of items) {
+  items.forEach((item, index) => {
     const itemElement = document.createElement('div');
     itemElement.classList.add('task-group');
     itemElement.innerHTML = `
       <input class="task-item" type="checkbox">
       <input class="task-item-text" placeholder="Task item...">
     `;
-
+  
     const myIcon = new Image();
     myIcon.src = More;
     myIcon.classList.add('task-item-more');
     itemElement.appendChild(myIcon);
-
+  
     const myDelete = new Image();
     myDelete.src = Delete;
     myDelete.classList.add('task-item-delete');
     itemElement.appendChild(myDelete);
-
+  
     const descriptionInput = itemElement.querySelector('.task-item-text');
     const completedInput = itemElement.querySelector('.task-item');
     const deleteButton = itemElement.querySelector('.task-item-delete');
-
+  
     descriptionInput.value = item.description;
     completedInput.checked = item.completed;
-
+  
     descriptionInput.addEventListener('change', () => {
       updateItem(item, 'description', descriptionInput.value);
     });
-
+  
     completedInput.addEventListener('change', () => {
       updateItem(item, 'completed', completedInput.checked);
     });
-
+  
     deleteButton.addEventListener('click', () => {
-      items = items.filter((i) => i !== item);
-      setItems(items);
-      refreshList();
+      deleteItem(index);
     });
-
+  
     postContainer.appendChild(itemElement);
-  }
+  });
 }
-
 
 addButton.addEventListener('click', () => {
   addItem();
-})
+});
 
 const addNewInput = document.getElementById('addnew');
 
-addNewInput.addEventListener('keydown', function(event) {
+addNewInput.addEventListener('keydown', function (event) {
   if (event.key === 'Enter') {
     event.preventDefault(); // Prevent the default form submission behavior
     addItem();
   }
 });
-
 
 refreshList();
